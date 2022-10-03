63b11204417f6e1d60dcc124a184d509f523d25b
chandraspt <acandra792@gmail.com>
2022-10-03 16:26:42 +0700

    menambah tugas 4

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -13,6 +13,19 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', [
+            'required' => 'Kode Matakuliah Harus diisi',
+            'min_lenght' => 'Kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]', [
+            'required' => 'Nama matakuliah Harus diisi',
+            'min_lenght' =>'Nama terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
         $data = [
             'kode' => $this->input->post('kode'),
             'nama' => $this->input->post('nama'),
@@ -21,5 +34,6 @@ class Matakuliah extends CI_Controller
 
         
         $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
