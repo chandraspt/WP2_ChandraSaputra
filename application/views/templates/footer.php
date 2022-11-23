be79e1a932e9e56c2b910a39d93949a7bb5ecf7d
chandraspt <acandra792@gmail.com>
2022-11-23 14:20:16 +0700

    tugas tugas 11

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -14,14 +14,13 @@
 </div>
 <!-- End of Page Wrapper -->
 
-<!-- Scroll to Top Button -->
+<!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
 
-<!-- Logout Modal -->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog"
-aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -32,20 +31,16 @@ aria-labelledby="exampleModalLabel" aria-hidden="true">
             </div>
             <div class="modal-body">Pilih "Logout" di bawah jika kamu yakin sudah selesai.</div>
             <div class="modal-footer">
-                <button class="btn btn-secondary" type="button"
-data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="<?=
-base_url('autentifikasi/logout'); ?>">Logout</a>
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="<?= base_url('autentifikasi/logout'); ?>">Logout</a>
             </div>
         </div>
     </div>
 </div>
 
 <!-- Bootstrap core JavaScript-->
-<script src="<?= base_url('assets/'); 
-?>vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url('assets/'); 
-?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
 <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
@@ -59,6 +54,23 @@ base_url('autentifikasi/logout'); ?>">Logout</a>
         $(this).next('.custom-file-label').addClass("selected").html(fileName);
     });
 
+
+    $('.form-check-input').on('click', function() {
+        const menuId = $(this).data('menu');
+        const roleId = $(this).data('role');
+
+        $.ajax({
+            url: "<?= base_url('admin/changeaccess'); ?>",
+            type: 'post',
+            data: {
+                menuId: menuId,
+                roleId: roleId
+            },
+            success: function() {
+                document.location.href = "<?= base_url('admin/akses-role/'); ?>" + roleId;
+            }
+        });
+    });
     $(document).ready(function() {
         $("#table-datatable").dataTable();
     });
